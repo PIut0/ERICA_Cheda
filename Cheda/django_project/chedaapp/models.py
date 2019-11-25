0a05ec1526a205664de9fc4b168fa4285d28a9b0
HyeseonLee <hyesun1999@gmail.com>
2019-11-25 23:54:36 +0900

    [Back-end] feat: Set model
    정리한 데이터들을 기반으로 항목들에 대한 model을 구성하였다.

--- Cheda/django_project/chedaapp/models.py
+++ Cheda/django_project/chedaapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    # locate # 장소->카테고리화
+    name = models.CharField(max_length=100) #강좌명
+    for_whom = models.CharField(max_length=50) #교육대상
+
+    L_FIELD_CHOICES = [
+        ('CF','문화예술/가정'), #Culture and Family
+        ('SP', '생활체육/건강'), #Sports
+        ('HS','인문/사회'), #Humanities and Social
+        ('IT','IT관련'), #IT
+        ('LC', '자격증과정'), #License
+        ('GN', '일반') #General
+    ]
+
+    l_field = models.CharField(max_length=2, choices=L_FIELD_CHOICES) #강좌분야
+
+    fee = models.IntegerField() #수강료
+    st_period = models.DateField(auto_now=False, auto_now_add=False) #교육기간_시작
+    fin_period = models.DateField(auto_now=False, auto_now_add=False) #교육기간_끝
+
+    date = models.TextField(max_length=200) #수강요일
+    where = models.CharField(max_length=100) #교육장소
+
+    to_st_date = models.DateTimeField(auto_now=False, auto_now_add=False) #접수기간_시작
+    to_st_date = models.DateTimeField(auto_now=False, auto_now_add=False) #접수기간_끝
+    
+    people=models.IntegerField() #수강정원
+
+    how_apply = models.CharField(max_length = 50) #접수방법
+    ask=models.CharField(max_length=50) #문의전화
+
+    etc=models.TextField(max_length=300) #비고
+
+
+
+
+
+
